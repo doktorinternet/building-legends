20c57da0cb7e1b7c8c026ef6943ed43576a6fbdf
Anton <wahlberg.anton@gmail.com>
2017-12-04 11:30:10 +0100

    Add project

---
+++ src/main/java/antonslakejer/buildinglegends/BuildinglegendsApplication.java
@@ -0,0 +1,12 @@
+package antonslakejer.buildinglegends;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildinglegendsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuildinglegendsApplication.class, args);
+	}
+}
---
+++ src/test/java/antonslakejer/buildinglegends/BuildinglegendsApplicationTests.java
@@ -0,0 +1,16 @@
+package antonslakejer.buildinglegends;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BuildinglegendsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
