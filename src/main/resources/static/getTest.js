2881e701fc61e9599f0b4eb18d7d6d488fadbd69
Anton <wahlberg.anton@gmail.com>
2017-12-08 10:03:16 +0100

    Catching up

--- src/main/resources/static/getTest.js
+++ src/main/resources/static/getTest.js
@@ -38,24 +38,6 @@ var itemSearchTags = {
     tags: "tags",
     tree: "tree"
 }
-<<<<<<< HEAD
-
-
-
-var champUrl = "https://eun1.api.riotgames.com/lol/static-data/v3/champions?locale=en_US";
-
-// https://eun1.api.riotgames.com/lol/static-data/v3/champions?locale=en_US&tags=image&tags=keys&tags=passive&tags=spells&tags=stats&tags=tags&dataById=false&api_key=RGAPI-9473df52-0728-4513-a12e-7aa5dd60093a
-// https://eun1.api.riotgames.com/lol/static-data/v3/champions/266?locale=en_US&tags=allytips&tags=blurb&tags=enemytips&tags=image&tags=info&tags=lore&tags=partype&tags=passive&tags=recommended&tags=skins&tags=spells&tags=stats&tags=tags&api_key=RGAPI-9473df52-0728-4513-a12e-7aa5dd60093a
-// https://eun1.api.riotgames.com/lol/static-data/v3/items?locale=en_US&tags=colloq&tags=consumeOnFull&tags=consumed&tags=depth&tags=effect&tags=from&tags=gold&tags=groups&tags=hideFromAll&tags=image&tags=inStore&tags=into&tags=maps&tags=requiredChampion&tags=sanitizedDescription&tags=specialRecipe&tags=stacks&tags=stats&tags=tags&tags=tree&api_key=RGAPI-9473df52-0728-4513-a12e-7aa5dd60093a
-// https://eun1.api.riotgames.com/lol/static-data/v3/items/1001?locale=en_US&api_key=RGAPI-9473df52-0728-4513-a12e-7aa5dd60093a
-
-var currentItemStats = {}; // For storing the stats of the selected item 
-var allItems = {}; // For caching later
-function getAllItems() {
-    var request = new XMLHttpRequest();
-    var requestURL = "http://ddragon.leagueoflegends.com/cdn/7.24.1/data/en_US/item.json";
-    request.open("GET", requestURL);
-=======
 
 var champUrl = "https://eun1.api.riotgames.com/lol/static-data/v3/champions?locale=en_US";
 
@@ -67,39 +49,27 @@ var champUrl = "https://eun1.api.riotgames.com/lol/static-data/v3/champions?loca
 var allItems = []; // For caching later
 function getAllItems() {
     var request = new XMLHttpRequest();
-    var requestURL =  "http://ddragon.leagueoflegends.com/cdn/7.24.1/data/en_US/item.json";
+    var requestURL = "http://ddragon.leagueoflegends.com/cdn/7.24.1/data/en_US/item.json";
     request.open("GET", requestURL, true);
->>>>>>> 4f39638858755629e130142b382362c716b0b1c9
     request.responseType = "json";
     request.send();
     request.onload = function () {
         let items = request.response;
-<<<<<<< HEAD
         stats = items["basic"]["stats"];
         console.log(items);
         allItems = items["data"];
         for (let item in allItems) {
-=======
-        let i = 0;
-        for (let item in items["data"]) {
-            allItems.push(item);
->>>>>>> 4f39638858755629e130142b382362c716b0b1c9
             let mySpan = document.createElement("span");
             let myImg = document.createElement("img");
             let myTitle = document.createElement("span");
             myImg.src = "http://ddragon.leagueoflegends.com/cdn/7.24.1/img/item/" + items["data"][item]["image"]["full"];
-<<<<<<< HEAD
-            myTitle.innerText = allItems[item]["name"];
-=======
             myTitle.innerText = items["data"][item]["name"];
->>>>>>> 4f39638858755629e130142b382362c716b0b1c9
             mySpan.appendChild(myImg);
             mySpan.appendChild(myTitle);
             document.getElementById("searchRow").appendChild(mySpan);
             i++;
         }
-<<<<<<< HEAD
-        extractStats("1001");
+        extractStats("3078");
     };
 }
 
@@ -111,7 +81,7 @@ function extractStats(itemID) {
         console.log(item);
     }
 
-    item["tags"].forEach( tag => {
+    item["tags"].forEach(tag => {
         if (tag.indexOf("Regen") > -1) {
             let desc = parseDescription(item);
             for (let prop in desc) {
@@ -159,22 +129,6 @@ function parseDescription(item) {
         "PercentMPRegenMod": mpregen
     };
 }
-=======
-        chooseItemListener(1);
-    };
-}
-
-function chooseItemListener(e){
-    allItems.forEach(item => {
-    //    console.log(item);
-        if(item.image.full.contains(e.id)) {
-            // Assign item to available item slot
-        }
-    });
-}
-
->>>>>>> 4f39638858755629e130142b382362c716b0b1c9
-
 
 function getAllChampions() {
     var request = new XMLHttpRequest();
@@ -185,8 +139,6 @@ function getAllChampions() {
     request.onload = function () {
         let champions = request.response;
         for (let champion in champions["data"]) {
-<<<<<<< HEAD
-=======
             let mySpan = document.createElement("span");
             let myImg = document.createElement("img");
             let myTitle = document.createElement("span");
@@ -208,7 +160,6 @@ function getOneChampion(event) {
     request.onload = function () {
         let champions = request.response;
         for (let champion in champions["data"]) {
->>>>>>> 4f39638858755629e130142b382362c716b0b1c9
             let mySpan = document.createElement("span");
             let myImg = document.createElement("img");
             let myTitle = document.createElement("span");
@@ -258,43 +209,20 @@ function loadJSON(callback) {
 
 }
 
-<<<<<<< HEAD
 function handleRunes(response) {
-=======
-
-function handleRunes(response){
->>>>>>> 4f39638858755629e130142b382362c716b0b1c9
     // Do Something with the response e.g.
     console.log(response);
     jsonresponse = JSON.parse(response);
     console.log(jsonresponse);
-<<<<<<< HEAD
     for (let perk in jsonresponse) {
-=======
-    for(let perk in jsonresponse){
->>>>>>> 4f39638858755629e130142b382362c716b0b1c9
 
         let mySpan = document.createElement("span");
         let myImg = document.createElement("img");
         let myTitle = document.createElement("span");
         myImg.src = jsonresponse.
-<<<<<<< HEAD
             myTitle.innerText = champions["data"][champion]["name"];
         mySpan.appendChild(myImg);
         mySpan.appendChild(myTitle);
     }
     document.getElementById("searchRow").appendChild(mySpan);
 }
-=======
-        myTitle.innerText = champions["data"][champion]["name"];
-        mySpan.appendChild(myImg);
-        mySpan.appendChild(myTitle);
-    }
-        document.getElementById("searchRow").appendChild(mySpan);
-}
-
-
-// http://ddragon.leagueoflegends.com/cdn/6.24.1/data/en_US/champion/Aatrox.json
-
-
->>>>>>> 4f39638858755629e130142b382362c716b0b1c9
